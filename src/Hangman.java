d7a8ca2b2e0972db64ba8769beb7b29d90563173
smit <w0849357@myacc.ca>
2025-01-28 20:57:31 -0800

    Started Assignment one - wrote code for hangman game

---
+++ src/Hangman.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // 1. Word list
+        String[] words = {"write", "that", "program", "java", "computer", "game"};
+
+        // 2. Game loop
+        String playAgain;
+        do {
+            // 2a. Pick a random word from the array
+            String secretWord = words[(int)(Math.random() * words.length)];
+
+            // 2b. Create a masked char array (all asterisks)
+            char[] maskedWord = new char[secretWord.length()];
+            for (int i = 0; i < maskedWord.length; i++) {
+                maskedWord[i] = '*';
+            }
+
+            int missedCount = 0; // track wrong guesses
+            // Keep guessing until user completes the word
+            while (!isWordGuessed(maskedWord)) {
+                System.out.print("(Guess) Enter a letter in word " + String.valueOf(maskedWord) + " > ");
+                char guess = input.next().toLowerCase().charAt(0);
+
+                // Check if the guess is correct
+                if (!revealLetter(secretWord, maskedWord, guess)) {
+                    // If revealLetter returns false, the guess was not in the word
+                    if (!alreadyGuessed(maskedWord, guess)) {
+                        missedCount++;
+                        System.out.println(guess + " is not in the word");
+                    } else {
+                        // If we get here, it means the letter is in the word,
+                        // but it was already revealed
+                        System.out.println(guess + " is already in the word");
+                    }
+                }
+            }
+
+            // Word fully guessed
+            System.out.println("The word is " + secretWord + ". You missed " + missedCount +
+                    (missedCount <= 1 ? " time" : " times"));
+
+            // 2c. Ask if the user wants to play again
+            System.out.print("Do you want to guess another word? Enter y or n> ");
+            playAgain = input.next();
+        } while (playAgain.equalsIgnoreCase("y"));
+
+        input.close();
+    }
+
+    /**
+     * Reveal `guess` in `maskedWord` if it appears in `secretWord`.
+     * @return true if at least one new letter was revealed, false if guess is not in the word.
+     */
+    public static boolean revealLetter(String secretWord, char[] maskedWord, char guess) {
+        boolean foundNewLetter = false;
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (secretWord.charAt(i) == guess && maskedWord[i] == '*') {
+                maskedWord[i] = guess;
+                foundNewLetter = true;
+            }
+        }
+        return foundNewLetter;
+    }
+
+    /**
+     * Check if the word is fully guessed (no asterisks left).
+     */
+    public static boolean isWordGuessed(char[] maskedWord) {
+        for (char c : maskedWord) {
+            if (c == '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if `guess` is already revealed in `maskedWord`.
+     */
+    public static boolean alreadyGuessed(char[] maskedWord, char guess) {
+        for (char c : maskedWord) {
+            if (c == guess) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
